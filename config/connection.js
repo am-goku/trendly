85c987066cfe8b8393420d79f330739aaed25f1f
Gokul Krishna <amkrishna2483@gmail.com>
2024-01-17 08:44:55 +0530

    moved connection uri to env

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 /* This code exports a function named `connect` that connects to a MongoDB database using Mongoose. The
 function uses the `mongoose.connect()` method to establish a connection to the database with the
 specified URL and options. If the connection is successful, it logs a message to the console. If
 there is an error, it logs an error message to the console. */
 module.exports.connect = function(){
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0', { dbName: 'trendly' })
-  .then(() => {
+mongoose.connect(process.env.ATLAS_URI)
+  .then((res) => {
     console.log('Connected to Database');
   })
   .catch((error) => {
